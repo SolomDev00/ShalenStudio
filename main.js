96f15f8ffee42cc213945704acf59b47981f50ba
Eslam Wael <84073355+SolomDev00@users.noreply.github.com>
2023-06-11 03:57:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -239,3 +239,18 @@ window.onscroll = () => {
     document.querySelector("#arrowHome i").style.right = "-40px";
   }
 };
+
+let bgLoading = document.getElementById("bgLoading");
+let bgLoadingCont = document.querySelector("#bgLoading .loader");
+window.onload = () => {
+  setTimeout(function () {
+    bgLoadingCont.style.opacity = "0";
+    bgLoading.style.pointerEvents = "none";
+    bgLoading.style.backdropFilter = "blur(0px)";
+    setTimeout(function () {
+      bgLoadingCont.style.display = "none";
+      bgLoading.style.display = "none";
+    }, 2500);
+  }, 2500);
+};
+
